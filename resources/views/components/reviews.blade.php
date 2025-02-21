4c183f55fe33c84980dc2099e5665c2973f29453
David Essien <davidessienshare@gmail.com>
2025-02-21 13:40:32 +0100

    update reviews section

--- resources/views/components/reviews.blade.php
+++ resources/views/components/reviews.blade.php
@@ -50,29 +50,31 @@
 
         <!-- Reviews Container -->
         <div class="relative">
-            <div class="flex mb-8 space-x-6">
+            <div class="flex flex-wrap mb-8 -mx-2">
                 <template x-for="(review, index) in reviews.slice(0, 4)" :key="index">
-                    <div class="w-full p-6 bg-white rounded-lg shadow-sm">
-                        <div class="flex items-center justify-between mb-4">
-                            <div class="flex items-center">
-                                <div class="flex items-center justify-center w-10 h-10 font-medium text-white bg-[#00AA00] rounded-full" x-text="review.initial">
+                    <div class="w-full p-2 md:w-1/2 lg:w-1/4">
+                        <div class="p-6 bg-white rounded-lg shadow-sm">
+                            <div class="flex items-center justify-between mb-4">
+                                <div class="flex items-center">
+                                    <div class="flex items-center justify-center w-10 h-10 font-medium text-white bg-[#00AA00] rounded-full" x-text="review.initial">
+                                    </div>
+                                    <div class="ml-3">
+                                        <h3 class="font-medium text-gray-900" x-text="review.name"></h3>
+                                        <p class="text-sm text-gray-500" x-text="review.date"></p>
+                                    </div>
                                 </div>
-                                <div class="ml-3">
-                                    <h3 class="font-medium text-gray-900" x-text="review.name"></h3>
-                                    <p class="text-sm text-gray-500" x-text="review.date"></p>
-                                </div>
+                                <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
+                                     alt="Google" class="h-5">
                             </div>
-                            <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
-                                 alt="Google" class="h-5">
+                            <div class="flex mb-3">
+                                <template x-for="star in 5">
+                                    <svg class="w-5 h-5 text-orange-400" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
+                                    </svg>
+                                </template>
+                            </div>
+                            <p class="text-gray-600" x-text="review.text"></p>
                         </div>
-                        <div class="flex mb-3">
-                            <template x-for="star in 5">
-                                <svg class="w-5 h-5 text-orange-400" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-                                </svg>
-                            </template>
-                        </div>
-                        <p class="text-gray-600" x-text="review.text"></p>
                     </div>
                 </template>
             </div>

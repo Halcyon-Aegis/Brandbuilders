9d169095e7f0ee7c2fe92e9e7820449830f8b5f6
David Essien <davidessienshare@gmail.com>
2025-02-21 14:46:17 +0100

    update services section

--- resources/views/components/services.blade.php
+++ resources/views/components/services.blade.php
@@ -1,5 +1,5 @@
-<section class="px-4 py-16 sm:px-6 lg:px-8 bg-gray-50">
-    <div class="mx-auto max-w-7xl">
+<section class="px-4 py-16 bg-white sm:px-6 lg:px-8">
+    <div class="container mx-auto">
         <p class="mb-4 text-lg tracking-wider text-center text-gray-500 uppercase">OUR CORE SERVICES</p>
         <div class="mb-16 text-center">
             <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl">
@@ -10,8 +10,8 @@
 
         <div class="grid gap-8 md:grid-cols-3">
             <!-- Build Website Card -->
-            <div class="flex flex-col items-center p-6 bg-white rounded-lg shadow-sm">
-                <div class="flex items-center justify-center w-20 h-20 mb-4 rounded-full bg-lime-50">
+            <div class="flex flex-col items-center p-12 bg-white rounded-lg shadow-sm hover:shadow-xl">
+                <div class="flex items-center justify-center w-20 h-20 mb-12 rounded-full bg-lime-100">
                     <img src="{{asset('img/browser-with-wheel-green.svg')}}" alt="Brand builders image" class="">
                 </div>
                 <h3 class="mb-2 text-3xl font-semibold text-gray-900">Build A Website</h3>
@@ -19,8 +19,8 @@
             </div>
 
             <!-- Fix Website Card -->
-            <div class="flex flex-col items-center p-6 bg-white rounded-lg shadow-sm">
-                <div class="flex items-center justify-center w-20 h-20 mb-4 bg-purple-100 rounded-full">
+            <div class="flex flex-col items-center p-12 bg-white rounded-lg shadow-sm hover:shadow-xl">
+                <div class="flex items-center justify-center w-20 h-20 mb-12 bg-purple-100 rounded-full">
                     <img src="{{asset('img/browser-with-shield-black.svg')}}" alt="Brand builders image" class="">
                 </div>
                 <h3 class="mb-2 text-3xl font-semibold text-gray-900">Fix Your Website</h3>
@@ -28,8 +28,8 @@
             </div>
 
             <!-- Brand Business Card -->
-            <div class="flex flex-col items-center p-6 bg-white rounded-lg shadow-sm">
-                <div class="flex items-center justify-center w-20 h-20 mb-4 bg-blue-100 rounded-full">
+            <div class="flex flex-col items-center p-6 bg-white rounded-lg shadow-sm hover:shadow-xl">
+                <div class="flex items-center justify-center w-20 h-20 mb-12 bg-blue-100 rounded-full">
                     <img src="{{asset('img/browser-with-wheel-green-black.svg')}}" alt="Brand builders image" class="">
                 </div>
                 <h3 class="mb-2 text-3xl font-semibold text-gray-900">Brand Your Business</h3>
